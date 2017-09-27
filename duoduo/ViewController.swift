265fddc6229028dcc20951aadbdea7b928164b01
cookcode.me <cookcode_me@sina.com>
2017-09-28 00:35:19 +0800

    Update demo

--- duoduo/ViewController.swift
+++ duoduo/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  duoduo
 //
-//  Created by linsirui on 2017/9/25.
+//  Created by William on 2017/9/25.
 //  Copyright © 2017年 duoduo. All rights reserved.
 //
 
@@ -36,7 +36,12 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 300) // TODO height
+        return CGSize(width: view.frame.width, height: 400) // TODO height
+    }
+    
+    // rotate handling
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView?.collectionViewLayout.invalidateLayout()
     }
 
 
@@ -62,7 +67,7 @@ class FeedCell: UICollectionViewCell {
             [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)])
         attributedText.append(NSAttributedString(string: "\n编程兔 126评论 16分钟前", attributes:
             [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 10),
-             NSForegroundColorAttributeName: UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1) ]))
+             NSForegroundColorAttributeName: UIColor.rgb(red: 155, green: 161, blue: 171)]))
         
         // line spacing
         let paragraphStyle = NSMutableParagraphStyle()
@@ -106,10 +111,41 @@ class FeedCell: UICollectionViewCell {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "kid_program_pic02")
         imageView.contentMode = .scaleAspectFill
-        //imageView.layer.masksToBounds = true
+        imageView.layer.masksToBounds = true
         return imageView
     }()
     
+    let likesCommentsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "488 点赞  107 评论"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = UIColor.rgb(red: 155, green: 161, blue: 171)
+        return label
+    }()
+    
+    
+    let deviderViewLine : UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(red: 226, green: 228, blue: 232)
+        return view
+    }()
+    
+    
+    
+    static func buttonForTitle(title: String, imageName: String) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(UIColor.rgb(red: 143, green: 150, blue: 163), for: .normal)
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 8, 0, 0)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        return button
+    }
+    
+    let likeButton: UIButton = FeedCell.buttonForTitle(title: "点赞", imageName: "heart-shape-outline")
+    let commentButton: UIButton = FeedCell.buttonForTitle(title: "评论", imageName: "comment-white-oval-bubble")
+    let shareButton: UIButton = FeedCell.buttonForTitle(title: "分享", imageName: "share-symbol")
+
     
     func setupViews() {
         backgroundColor = UIColor.white
@@ -118,15 +154,43 @@ class FeedCell: UICollectionViewCell {
         addSubview(profileImageView)
         addSubview(statusTextView)
         addSubview(statusImageView)
+        addSubview(likesCommentsLabel)
+        addSubview(deviderViewLine)
+        addSubview(likeButton)
+        addSubview(commentButton)
+        addSubview(shareButton)
         
         // Constraints of Auto lay out
         // TODO fine tune auto lay out constraints
+        
+        addConstraintsWithFormat(format: "V:|-12-[v0]", views: nameLabel)
+
+    
         addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
         addConstraintsWithFormat(format: "H:|-4-[v0]-4-|", views: statusTextView)
-        addConstraintsWithFormat(format: "H:|[v0(200)]", views: statusImageView)
-        addConstraintsWithFormat(format: "V:|-12-[v0]", views: nameLabel)
-        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2(200)]|", views: profileImageView, statusTextView, statusImageView)
         
+        addConstraintsWithFormat(format: "H:|[v0]", views: statusImageView)
+        
+        addConstraintsWithFormat(format: "H:|-12-[v0]|", views: likesCommentsLabel)
+        
+        addConstraintsWithFormat(format: "H:|-12-[v0]-12-|", views: deviderViewLine)
+        
+        // let "likeButton", "commentButton" and "shareButton" in one line; how/why ??
+        addConstraintsWithFormat(format: "H:|[v0(v2)][v1(v2)][v2]|", views: likeButton, commentButton, shareButton)
+        
+        
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]-8-[v3(24)]-8-[v4(0.4)][v5(44)]|", views: profileImageView, statusTextView, statusImageView, likesCommentsLabel, deviderViewLine, likeButton)
+        
+        // cosntraints with bottom
+        addConstraintsWithFormat(format: "V:[v0(44)]|", views: commentButton)
+        addConstraintsWithFormat(format: "V:[v0(44)]|", views: shareButton)
+        
+    }
+}
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
 }
 
